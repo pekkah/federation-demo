e2c0ddaa6ea984daf23afc809e1d8190326fdd86
Pekka Heikura <pekkah@gmail.com>
2020-10-26 16:52:10 +0200

    Use tanka service address

--- gateway.js
+++ gateway.js
@@ -1,3 +1,6 @@
+// DO NOT DO THIS IN PRODUCTION IT DISABLES THE CERTIFICATE CHECK
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const { ApolloServer } = require("apollo-server");
 const { ApolloGateway } = require("@apollo/gateway");
 
@@ -9,13 +12,15 @@ const gateway = new ApolloGateway({
   // real usage-based metrics.
   serviceList: [
     { name: "accounts", url: "http://localhost:4001/graphql" },
-    { name: "reviews", url: "http://localhost:4002/graphql" },
     { name: "products", url: "http://localhost:4003/graphql" },
-    { name: "inventory", url: "http://localhost:4004/graphql" }
+    { name: "inventory", url: "http://localhost:4004/graphql" },
+
+    // we add our Tanka GraphQL reviews service here
+    { name: "reviews", url: "https://localhost:5001/graphql" },
   ],
 
   // Experimental: Enabling this enables the query plan view in Playground.
-  __exposeQueryPlanExperimental: false,
+  __exposeQueryPlanExperimental: true,
 });
 
 (async () => {
